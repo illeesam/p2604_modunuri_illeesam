f1360481a2b8360be86a1d2a3222007b68f98587
송성일 <illeesam@gmail.com>
2026-04-15 19:11:46 +0900

    111

--- ec_v26/shopjoy_v260406/_doc/ddl_pgsql/sy/sy_vendor.sql
+++ ec_v26/shopjoy_v260406/_doc/ddl_pgsql/sy/sy_vendor.sql
@@ -1,57 +1,73 @@
 -- ============================================================
--- sy_vendor : 업체 (판매업체 / 배송업체)
+-- sy_biz : 사업자 (사업체/법인)
 -- ID 규칙: YYMMDDhhmmss + random(4) = VARCHAR(16)
 -- ============================================================
-CREATE TABLE sy_vendor (
-    vendor_id       VARCHAR(16)     NOT NULL,
+CREATE TABLE sy_biz (
+    biz_id          VARCHAR(16)     NOT NULL,
     site_id         VARCHAR(16),                            -- sy_site.site_id
-    vendor_type     VARCHAR(20)     NOT NULL,               -- SELLER/COURIER/BOTH
-    vendor_nm       VARCHAR(100)    NOT NULL,
-    vendor_code     VARCHAR(50),
-    ceo             VARCHAR(50),
-    biz_no          VARCHAR(20),
+    biz_no          VARCHAR(20)     NOT NULL,               -- 사업자등록번호 (123-45-67890)
+    corp_no         VARCHAR(20),                            -- 법인등록번호 (선택)
+    biz_nm          VARCHAR(100)    NOT NULL,               -- 상호 / 회사명
+    biz_nm_en       VARCHAR(100),                           -- 영문 상호
+    ceo_nm          VARCHAR(50),                            -- 대표자명
+    biz_type        VARCHAR(50),                            -- 업태 (예: 도소매)
+    biz_item        VARCHAR(100),                           -- 종목 (예: 의류, 잡화)
+    biz_class_cd    VARCHAR(20),                            -- 코드: BIZ_CLASS (개인/법인/면세/간이)
+    zip_code        VARCHAR(10),
+    addr            VARCHAR(200),
+    addr_detail     VARCHAR(200),
     phone           VARCHAR(20),
     fax             VARCHAR(20),
     email           VARCHAR(100),
-    zip_code        VARCHAR(10),
-    address         VARCHAR(300),
-    contract_date   DATE,
-    contract_end    DATE,
-    status_cd       VARCHAR(20)     DEFAULT 'ACTIVE',       -- 코드: VENDOR_STATUS
-    bank_name       VARCHAR(50),
-    bank_account    VARCHAR(50),
-    bank_holder     VARCHAR(50),
-    memo            TEXT,
+    homepage        VARCHAR(200),
+    bank_nm         VARCHAR(50),                            -- 은행명
+    bank_account    VARCHAR(50),                            -- 계좌번호
+    bank_holder     VARCHAR(50),                            -- 예금주
+    biz_reg_url     VARCHAR(500),                           -- 사업자등록증 첨부 URL
+    open_date       DATE,                                   -- 개업일자
+    contract_date   DATE,                                   -- 계약일자
+    status_cd       VARCHAR(20)     DEFAULT 'ACTIVE',       -- 코드: BIZ_STATUS (ACTIVE/SUSPENDED/TERMINATED)
+    disp_path       VARCHAR(200),                           -- 점(.) 구분 표시경로
+    remark          VARCHAR(500),
     reg_by          VARCHAR(16),
     reg_date        TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
     upd_by          VARCHAR(16),
     upd_date        TIMESTAMP,
-    disp_path       VARCHAR(200),                           -- 점(.) 구분 표시경로
-    PRIMARY KEY (vendor_id)
+    PRIMARY KEY (biz_id),
+    UNIQUE (biz_no)
 );
 
-COMMENT ON TABLE  sy_vendor                IS '업체';
-COMMENT ON COLUMN sy_vendor.vendor_id      IS '업체ID (YYMMDDhhmmss+rand4)';
-COMMENT ON COLUMN sy_vendor.site_id        IS '사이트ID (sy_site.site_id)';
-COMMENT ON COLUMN sy_vendor.vendor_type    IS '업체유형 (SELLER/COURIER/BOTH)';
-COMMENT ON COLUMN sy_vendor.vendor_nm      IS '업체명';
-COMMENT ON COLUMN sy_vendor.vendor_code    IS '업체코드';
-COMMENT ON COLUMN sy_vendor.ceo            IS '대표자명';
-COMMENT ON COLUMN sy_vendor.biz_no         IS '사업자번호';
-COMMENT ON COLUMN sy_vendor.phone          IS '전화번호';
-COMMENT ON COLUMN sy_vendor.fax            IS '팩스';
-COMMENT ON COLUMN sy_vendor.email          IS '이메일';
-COMMENT ON COLUMN sy_vendor.zip_code       IS '우편번호';
-COMMENT ON COLUMN sy_vendor.address        IS '주소';
-COMMENT ON COLUMN sy_vendor.contract_date  IS '계약시작일';
-COMMENT ON COLUMN sy_vendor.contract_end   IS '계약종료일';
-COMMENT ON COLUMN sy_vendor.status_cd      IS '상태 (코드: VENDOR_STATUS)';
-COMMENT ON COLUMN sy_vendor.bank_name      IS '은행명';
-COMMENT ON COLUMN sy_vendor.bank_account   IS '계좌번호';
-COMMENT ON COLUMN sy_vendor.bank_holder    IS '예금주';
-COMMENT ON COLUMN sy_vendor.memo           IS '메모';
-COMMENT ON COLUMN sy_vendor.reg_by         IS '등록자 (sy_user.user_id)';
-COMMENT ON COLUMN sy_vendor.reg_date       IS '등록일';
-COMMENT ON COLUMN sy_vendor.upd_by         IS '수정자 (sy_user.user_id)';
-COMMENT ON COLUMN sy_vendor.upd_date       IS '수정일';
-COMMENT ON COLUMN sy_vendor.disp_path IS '점(.) 구분 표시경로 (트리 빌드용)';
+COMMENT ON TABLE  sy_biz                IS '사업자 (사업체/법인)';
+COMMENT ON COLUMN sy_biz.biz_id         IS '사업자ID (YYMMDDhhmmss+rand4)';
+COMMENT ON COLUMN sy_biz.site_id        IS '사이트ID (sy_site.site_id)';
+COMMENT ON COLUMN sy_biz.biz_no         IS '사업자등록번호';
+COMMENT ON COLUMN sy_biz.corp_no        IS '법인등록번호 (선택)';
+COMMENT ON COLUMN sy_biz.biz_nm         IS '상호 / 회사명';
+COMMENT ON COLUMN sy_biz.biz_nm_en      IS '영문 상호';
+COMMENT ON COLUMN sy_biz.ceo_nm         IS '대표자명';
+COMMENT ON COLUMN sy_biz.biz_type       IS '업태';
+COMMENT ON COLUMN sy_biz.biz_item       IS '종목';
+COMMENT ON COLUMN sy_biz.biz_class_cd   IS '사업자구분 (코드: BIZ_CLASS)';
+COMMENT ON COLUMN sy_biz.zip_code       IS '우편번호';
+COMMENT ON COLUMN sy_biz.addr           IS '주소';
+COMMENT ON COLUMN sy_biz.addr_detail    IS '상세주소';
+COMMENT ON COLUMN sy_biz.phone          IS '대표 전화';
+COMMENT ON COLUMN sy_biz.fax            IS '팩스';
+COMMENT ON COLUMN sy_biz.email          IS '대표 이메일';
+COMMENT ON COLUMN sy_biz.homepage       IS '홈페이지';
+COMMENT ON COLUMN sy_biz.bank_nm        IS '은행명';
+COMMENT ON COLUMN sy_biz.bank_account   IS '계좌번호';
+COMMENT ON COLUMN sy_biz.bank_holder    IS '예금주';
+COMMENT ON COLUMN sy_biz.biz_reg_url    IS '사업자등록증 첨부 URL';
+COMMENT ON COLUMN sy_biz.open_date      IS '개업일자';
+COMMENT ON COLUMN sy_biz.contract_date  IS '계약일자';
+COMMENT ON COLUMN sy_biz.status_cd      IS '상태 (코드: BIZ_STATUS)';
+COMMENT ON COLUMN sy_biz.disp_path      IS '점(.) 구분 표시경로';
+COMMENT ON COLUMN sy_biz.remark         IS '비고';
+COMMENT ON COLUMN sy_biz.reg_by         IS '등록자 (sy_user.user_id)';
+COMMENT ON COLUMN sy_biz.reg_date       IS '등록일';
+COMMENT ON COLUMN sy_biz.upd_by         IS '수정자 (sy_user.user_id)';
+COMMENT ON COLUMN sy_biz.upd_date       IS '수정일';
+
+CREATE INDEX idx_sy_biz_site   ON sy_biz (site_id);
+CREATE INDEX idx_sy_biz_status ON sy_biz (status_cd);
