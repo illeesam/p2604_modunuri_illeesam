084bfdbdb22e51352277cf39738f32410ffbc73f
송성일 <illeesam@gmail.com>
2026-05-03 10:24:50 +0900

    11

--- ec_v26/shopjoy_v260406/pages/fo/Home01.js
+++ ec_v26/shopjoy_v260406/pages/fo/Home01.js
@@ -86,7 +86,6 @@ window.Home01 = {
       .home-blog-grid { display:grid; grid-template-columns:repeat(auto-fill,minmax(300px,1fr)); gap:20px; }
       `;
       document.head.appendChild(s);
-      }
       startBannerTimer();
     });
     onBeforeUnmount(() => clearInterval(bannerTimer));
